832e895437fad8454678d856257a3ede47835ff1
matijaspan <matija.bulat@spandevacademy.onmicrosoft.com>
2022-08-22 00:03:06 +0200

    db slight improvements

--- CaffiatoDatabase.sql
+++ CaffiatoDatabase.sql
@@ -20,7 +20,7 @@ DROP TABLE dbo.Challenge;
 
 --Create tables
 CREATE TABLE dbo.UserCaffe(
-	ID int IDENTITY(1,1) NOT NULL,
+	IDUserCaffe int IDENTITY(1,1) NOT NULL,
 	Email nvarchar(1024) NOT NULL,
 	Username nvarchar(1024),
 	FirstName nvarchar(1024) NOT NULL,
@@ -28,65 +28,65 @@ CREATE TABLE dbo.UserCaffe(
 	DateOfBirth dateTime,
 	Oib varchar(11),
 	Points int NOT NULL DEFAULT 0,
-	CONSTRAINT PK_UserCaffe PRIMARY KEY CLUSTERED(ID)
+	CONSTRAINT PK_UserCaffe PRIMARY KEY CLUSTERED(IDUserCaffe)
 );
 GO
 
 CREATE TABLE dbo.Caffe(
-	ID int IDENTITY(1,1) NOT NULL,
+	IDCaffe int IDENTITY(1,1) NOT NULL,
 	[Name] nvarchar(1024) NOT NULL,
 	UserCaffeID int NOT NULL,
-	CONSTRAINT PK_Caffe PRIMARY KEY CLUSTERED(ID),
-	CONSTRAINT FK_UserCaffeCaffe FOREIGN KEY (UserCaffeID) REFERENCES UserCaffe(ID)
+	CONSTRAINT PK_Caffe PRIMARY KEY CLUSTERED(IDCaffe),
+	CONSTRAINT FK_UserCaffeCaffe FOREIGN KEY (UserCaffeID) REFERENCES UserCaffe(IDUserCaffe)
 );
 GO
 
 CREATE TABLE dbo.[Address] (
-	ID int IDENTITY(1,1) NOT NULL,
+	IDAddress int IDENTITY(1,1) NOT NULL,
 	StreetNumber nvarchar(1024) NOT NULL,
 	StreetName nvarchar(1024) NOT NULL,
 	City nvarchar(1024) NOT NULL,
 	PostCode nvarchar(1024),
 	CaffeID int NOT NULL,
-	CONSTRAINT PK_Address PRIMARY KEY CLUSTERED(ID),
-	CONSTRAINT FK_CaffeAddress FOREIGN KEY (CaffeID) REFERENCES Caffe(ID)
+	CONSTRAINT PK_Address PRIMARY KEY CLUSTERED(IDAddress),
+	CONSTRAINT FK_CaffeAddress FOREIGN KEY (CaffeID) REFERENCES Caffe(IDCaffe)
 );
 GO
 
 CREATE TABLE dbo.Deal (
-	ID int IDENTITY(1,1) NOT NULL,
+	IDDeal int IDENTITY(1,1) NOT NULL,
 	[Name] nvarchar(1024) NOT NULL,
 	[DateTime] dateTime NOT NULL,
 	Points int NOT NULL,
 	Price money NOT NULL,
 	CaffeID int NOT NULL,
 	Active bit DEFAULT 0,
-	CONSTRAINT PK_Deal PRIMARY KEY CLUSTERED(ID),
-	CONSTRAINT FK_CaffeDeal FOREIGN KEY (CaffeID) REFERENCES Caffe(ID)
+	CONSTRAINT PK_Deal PRIMARY KEY CLUSTERED(IDDeal),
+	CONSTRAINT FK_CaffeDeal FOREIGN KEY (CaffeID) REFERENCES Caffe(IDCaffe)
 );
 GO
 
 CREATE TABLE dbo.Transact (
-	ID int IDENTITY(1,1) NOT NULL,
+	IDTransact int IDENTITY(1,1) NOT NULL,
 	[Time] dateTime NOT NULL,
 	Amount money NOT NULL,
 	UserCaffeID int NOT NULL,
-	CONSTRAINT PK_Transact PRIMARY KEY CLUSTERED(ID),
-	CONSTRAINT FK_UserCaffeTransact FOREIGN KEY (UserCaffeID) REFERENCES UserCaffe(ID)
+	CONSTRAINT PK_Transact PRIMARY KEY CLUSTERED(IDTransact),
+	CONSTRAINT FK_UserCaffeTransact FOREIGN KEY (UserCaffeID) REFERENCES UserCaffe(IDUserCaffe)
 );
 GO
 
 CREATE TABLE dbo.Feedback (
-	ID int IDENTITY(1,1) NOT NULL,
+	IDFeedback int IDENTITY(1,1) NOT NULL,
 	FeedbackLog nvarchar(max),
-	CONSTRAINT PK_Feedback PRIMARY KEY CLUSTERED(ID)
+	CONSTRAINT PK_Feedback PRIMARY KEY CLUSTERED(IDFeedBack)
 );
 GO
 
 CREATE TABLE dbo.Challenge (
-	ID int IDENTITY(1,1) NOT NULL,
+	IDChallenge int IDENTITY(1,1) NOT NULL,
 	[Name] nvarchar(1024),
 	[Description] nvarchar(max),
-	CONSTRAINT PK_Challenge PRIMARY KEY CLUSTERED(ID)
+	CONSTRAINT PK_Challenge PRIMARY KEY CLUSTERED(IDChallenge)
 );
 GO
